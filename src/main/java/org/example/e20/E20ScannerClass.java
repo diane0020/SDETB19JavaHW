088499c089f72405ea25b293361bacd84f5112d2
Diane Bangal <diane.soliven@gmail.com>
2024-02-28 08:05:41 +0800

    Fixed E20Scanner Test Homework is finished and passed all the test.

--- src/main/java/org/example/e20/E20ScannerClass.java
+++ src/main/java/org/example/e20/E20ScannerClass.java
@@ -6,9 +6,9 @@ public class E20ScannerClass {
     public static void main(String[] args) {
         Scanner user = new Scanner(System.in);
 
-        System.out.print("Please Enter First Name");
+        System.out.println("Please Enter First Name");
         String firstName = user.nextLine();
-        System.out.print("Please Enter Last Name");
+        System.out.println("Please Enter Last Name");
         String lastName = user.nextLine();
         System.out.print(firstName + " " + lastName);
     }
--- src/test/java/e20Test/E20ScannerClassTest.java
+++ src/test/java/e20Test/E20ScannerClassTest.java
@@ -13,48 +13,43 @@ import java.io.PrintStream;
 import static org.junit.Assert.assertEquals;
 
 public class E20ScannerClassTest {
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final PrintStream originalOut = System.out;
-    private final ByteArrayInputStream inContent = new ByteArrayInputStream("John Doe\n".getBytes());
-    private final InputStream originalIn = System.in;
-    @Before
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
-        System.setIn(inContent);
+    private InputStream originalSystemIn;
+    private ByteArrayOutputStream outputStream;
+
+
+    public void setUpInputOutput() {
+        originalSystemIn = System.in;
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
     }
-    @After
-    public void restoreStreams() {
-        System.setOut(originalOut);
-        System.setIn(originalIn);
+
+
+    public void restoreSystemInAndOut() {
+        System.setIn(originalSystemIn);
+        System.setOut(System.out);
     }
 
     @Test
-    public void testMainMethod() {
-
-            String[] args = {}; // No arguments needed for this test
-        E20ScannerClass.main(new String[]{});
-            String expectedOutput = "Please Enter First Name\nPlease Enter Last Name\nJohn Doe\n";
-            assertEquals(expectedOutput, outContent.toString());
-        }
-
-
-
-        // Define the expected output
-        //String expectedOutput = "Please Enter First Name\nPlease Enter Last Name\nCindy Crawford";
-        String failureMessage ="Write a program that takes user's first name and last name and prints in console\n" +
-                "Instruct the user to enter first name: \"Please Enter First Name\"\n" +
-                "Capture the first name\n" +
-                "Instruct the user to enter last name:\"Please Enter Last Name\"\n" +
-                "Capture last name\n" +
-                "Print first name and last name\n" +
-                "Final Output:\n" +
-                "Example:\n" +
-                "Please Enter First Name\n" +
-                "Please Enter Last Name\n" +
-                "Cindy Crawford";
-
-        // Compare the concatenated string with the expected output
-        //assertEquals(failureMessage,expectedOutput, output.toString());
-
+    public void testUserNameInput() {
+        setUpInputOutput();
+        setSystemIn("John\nDoe\n"); // Simulate user input of 'John' and 'Doe'
+        E20ScannerClass.main(new String[0]);
+        assertEquals("Please Enter First Name\nPlease Enter Last Name\nJohn Doe", getOutput().replace("\r\n", "\n"));
+        restoreSystemInAndOut();
     }
+
+    private void setSystemIn(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+    }
+
+    private String getOutput() {
+        return outputStream.toString();
+    }
+
+
+
+
+
+
+}
 
